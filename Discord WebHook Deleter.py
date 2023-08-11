ad2fc36fe1b9fb738af0b4883d04f5687441762e
TimelessOn15Hz <timeless.off.tv@gmail.com>
2023-08-11 12:41:47 +0300

    Function redesign & separate banner file

--- Discord WebHook Deleter.py
+++ Discord WebHook Deleter.py
@@ -15,34 +15,33 @@ from banner import banner
 
 init()
 os.system("title WEBHOOK DELETER")
-banner = (Fore.MAGENTA + """
-  █     █░▓█████  ▄▄▄▄    ██░ ██  ▒█████   ▒█████   ██ ▄█▀   ▓█████▄ ▓█████  ██▓    ▓█████▄▄▄█████▓▓█████  ██▀███
- ▓█░ █ ░█░▓█   ▀ ▓█████▄ ▓██░ ██▒▒██▒  ██▒▒██▒  ██▒ ██▄█▒    ▒██▀ ██▌▓█   ▀ ▓██▒    ▓█   ▀▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒
- ▒█░ █ ░█ ▒███   ▒██▒ ▄██▒██▀▀██░▒██░  ██▒▒██░  ██▒▓███▄░    ░██   █▌▒███   ▒██░    ▒███  ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒
- ░█░ █ ░█ ▒▓█  ▄ ▒██░█▀  ░▓█ ░██ ▒██   ██░▒██   ██░▓██ █▄    ░▓█▄   ▌▒▓█  ▄ ▒██░    ▒▓█  ▄░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄
- ░░██▒██▓ ░▒████▒░▓█  ▀█▓░▓█▒░██▓░ ████▓▒░░ ████▓▒░▒██▒ █▄   ░▒████▓ ░▒████▒░██████▒░▒████▒ ▒██▒ ░ ░▒████▒░██▓ ▒██▒
- ░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒ ▒ ░░▒░▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒    ▒▒▓  ▒ ░░ ▒░ ░░ ▒░▓  ░░░ ▒░ ░ ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░
-   ▒ ░ ░   ░ ░  ░▒░▒   ░  ▒ ░▒░ ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░    ░ ▒  ▒  ░ ░  ░░ ░ ▒  ░ ░ ░  ░   ░     ░ ░  ░  ░▒ ░ ▒░
-   ░   ░     ░    ░    ░  ░  ░░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░     ░ ░  ░    ░     ░ ░      ░    ░         ░     ░░   ░
-     ░       ░  ░ ░       ░  ░  ░    ░ ░      ░ ░  ░  ░         ░       ░  ░    ░  ░   ░  ░           ░  ░   ░
-""" + Fore.LIGHTCYAN_EX)
-print(banner)
-webhook = input(" [INPUT] ENTER THE WEBHOOK TO DELETE : ")
+banner = (Fore.RED + banner)
 
-def delete():
-    requests.delete(webhook)
-    check = requests.get(webhook)
-    if check.status_code == 404:
-        print("\n [LOGS] WEBHOOK SUCCESFULLY DELETED")
-        os.system("pause >nul")  # Pause command in Batch (press any key to exit the code)
-    elif check.status_code == 200:
-        print("\n [LOGS] FAILED TO DELETE WEBHOOK")
+def main():
+    print(banner)
+    webhook = input(" [INPUT] ENTER WEBHOOK URL : ")
+
+    test = requests.get(webhook)
+    if test.status_code == 404:
+        print("\n [OUTPUT] THE WEBHOOK IS INVALID")
+        print("\n PRESS ANY KEY TO EXIT")
         os.system("pause >nul")
+    elif test.status_code == 200:
+        print("\n [OUTPUT] WEBHOOK IS VALID -- DELETING WEBHOOK")
+        delete()
 
-test = requests.get(webhook)
-if test.status_code == 404:
-    print("\n [LOGS] THE WEBHOOK IS INVALID")
-    os.system("pause >nul")
-elif test.status_code == 200:
-    print("\n [LOGS] THE WEBHOOK IS VALID")
-    delete()
+    def delete():
+        requests.delete(webhook)
+        check = requests.get(webhook)
+        status = check.status_code
+        if status == 404:
+            print("\n [OUTPUT] WEBHOOK SUCCESFULLY DELETED")
+            print("\n PRESS ANY KEY TO EXIT")
+            os.system("pause >nul")  # Pause command in Batch (press any key to exit the code)
+        elif status == 200:
+            print(f"\n [OUTPUT] FAILED TO DELETE WEBHOOK -- STATUS CODE {status}")
+            print("\n PRESS ANY KEY TO EXIT")
+            os.system("pause >nul")
+
+if __name__ == '__main__':
+    main()
